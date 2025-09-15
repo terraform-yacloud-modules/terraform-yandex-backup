a3519040b073118eedc847ea616f770c735a854a
Anton <patsev.anton@gmail.com>
2025-09-15 18:56:05 +0600

    acticate daily_backup_policy

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -56,34 +56,34 @@ module "iam_accounts" {
 
 }
 
-# module "daily_backup_policy" {
-#   # Путь к папке с файлами модуля
-#   source = "../.."
+module "daily_backup_policy" {
+  # Путь к папке с файлами модуля
+  source = "../.."
 
-#   # Переопределяем только необходимые параметры
-#   name = "my-daily-backup-policy"
+  # Переопределяем только необходимые параметры
+  name = "my-daily-backup-policy"
 
-#   scheduling = {
-#     enabled              = true
-#     max_parallel_backups = 2
-#     random_max_delay     = "15m"
-#     scheme               = "ALWAYS_INCREMENTAL"
-#     weekly_backup_day    = "MONDAY" # не используется в DAILY, но должен быть указан
-#     backup_sets = [
-#       {
-#         type      = "DAILY"
-#         repeat_at = ["02:00"] # Ежедневно в 2 часа ночи
-#       }
-#     ]
-#   }
+  scheduling = {
+    enabled              = true
+    max_parallel_backups = 2
+    random_max_delay     = "15m"
+    scheme               = "ALWAYS_INCREMENTAL"
+    weekly_backup_day    = "MONDAY" # не используется в DAILY, но должен быть указан
+    backup_sets = [
+      {
+        type      = "DAILY"
+        repeat_at = ["02:00"] # Ежедневно в 2 часа ночи
+      }
+    ]
+  }
 
-#   retention = {
-#     after_backup = true
-#     rules = [
-#       {
-#         # Хранить копии, сделанные за последние 14 дней
-#         max_age = "14d"
-#       }
-#     ]
-#   }
-# }
+  retention = {
+    after_backup = true
+    rules = [
+      {
+        # Хранить копии, сделанные за последние 14 дней
+        max_age = "14d"
+      }
+    ]
+  }
+}
